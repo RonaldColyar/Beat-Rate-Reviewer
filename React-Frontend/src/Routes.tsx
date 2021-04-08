d1a00396eb23581727066c43c3a423b991a04b59
RonaldColyar <phantexcreations@gmail.com>
2021-04-08 04:29:46 -0500

    minor markup

--- React-Frontend/src/Routes.tsx
+++ React-Frontend/src/Routes.tsx
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router,Route} from "react-router-dom";
-
+import DesktopHome from "../src/Screens/DesktopHome";
+import Mobilehome from "../src/Screens/Mobilehome";
 export var loggedInRoutes= 
     <Router>
 
-        <Route>
+        <Route exact path = "/">
+            {window.screen.width > 800? DesktopHome : Mobilehome }
             
         </Route>
 
--- React-Frontend/src/Screens/DesktopHome.tsx
+++ React-Frontend/src/Screens/DesktopHome.tsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import ConfirmReview from '../Components/ConfirmReview'
+import ReviewPopup from '../Components/ReviewPopup'
+import SongSelectorComp from '../Components/SongSelectorComp'
 
 export default class DesktopHome extends Component {
     render() {
         return (
             <div>
-                
+                <SongSelectorComp/>
+                <ConfirmReview/>
+                <ReviewPopup/>
             </div>
         )
     }
